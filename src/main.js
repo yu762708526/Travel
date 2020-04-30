a2f874646b57a15cfff5371dbeae5e61f78d2468
Sakura <762708526@qq.com>
2020-04-30 17:57:38 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '../mint'
 
+// 引入全部组件
+// import Mint from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+// Vue.use(Mint)
 Vue.config.productionTip = false
 
 new Vue({
