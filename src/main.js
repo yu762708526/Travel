8258c6d3c2df674f6d176dd2ee533d966343998b
Sakura <762708526@qq.com>
2020-05-09 21:36:21 +0800

    安装了axios

--- src/main.js
+++ src/main.js
@@ -9,10 +9,13 @@ import '../src/assets/styles/border.css'
 import '../src/assets/styles/iconfont.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
+import axios from 'axios'
+import vueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
+Vue.use(vueAxios, axios)
 
 new Vue({
   router,
